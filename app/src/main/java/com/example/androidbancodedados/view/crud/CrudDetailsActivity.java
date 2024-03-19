1fcba68ddc983dd77a27fb8f11527a1fcbf773db
Luiz Furmann <furmannluiz@gmail.com>
2024-03-19 08:08:58 -0300

    adicionando edição de campos na tela de detalhes

--- app/src/main/java/com/example/androidbancodedados/view/crud/CrudDetailsActivity.java
+++ app/src/main/java/com/example/androidbancodedados/view/crud/CrudDetailsActivity.java
@@ -1,13 +1,15 @@
 package com.example.androidbancodedados.view.crud;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
+import com.example.androidbancodedados.R;
 import com.example.androidbancodedados.data.SqlHelper;
 import com.example.androidbancodedados.databinding.ActivityCrudDetailsBinding;
 import com.example.androidbancodedados.model.Contact;
@@ -33,6 +35,7 @@ public class CrudDetailsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         if (intent.getSerializableExtra("contact") != null) {
             Contact contact = (Contact) getIntent().getSerializableExtra("contact");
+            binding.btnSave.setVisibility(View.GONE);
 
             binding.etName.setText(contact.getName());
             binding.etContact.setText(contact.getPhoneNumber());
@@ -58,5 +61,28 @@ public class CrudDetailsActivity extends AppCompatActivity {
                     Toast.makeText(CrudDetailsActivity.this, "Contato salvo", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.option_delete){
+
+        }
+        if(item.getItemId() == R.id.option_edit){
+            binding.etName.setEnabled(true);
+            binding.etContact.setEnabled(true);
+            binding.btnSave.setVisibility(View.VISIBLE);
+        }
+        switch (item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
     }
 }
